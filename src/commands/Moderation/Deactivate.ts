05854a2c73a94230ff2ac1d61a80a5084a397994
DEVILL-MASCOT <102029388+DEVILL-MASCOT@users.noreply.github.com>
2022-03-21 17:35:33 +0530

    Update Deactivate.ts

--- src/commands/Moderation/Deactivate.ts
+++ src/commands/Moderation/Deactivate.ts
@@ -6,7 +6,7 @@ import { IParsedArgs, ISimplifiedMessage } from '../../typings'
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
-            adminOnly: true,
+            modsOnly: true,
             command: 'deactivate',
             aliases: ['deact'],
             description: 'deactivate certain features on group-chats',
