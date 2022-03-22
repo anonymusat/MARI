47fffdd05242b60334d3909915c37dfc74602f9f
DEVILL-MASCOT <102029388+DEVILL-MASCOT@users.noreply.github.com>
2022-03-22 15:11:50 +0530

    Update Sticker.ts

--- src/commands/Utils/Sticker.ts
+++ src/commands/Utils/Sticker.ts
@@ -80,8 +80,8 @@ export default class Command extends BaseCommand {
 					})();
 					return {
 						categories,
-						pack: pack[1] || "⚡ Here you go ",
-						author: pack[2] || "Yotsuba ⚡",
+						pack: pack[1] || "FOR YOU MY LOVE 🥰😘 ",
+						author: pack[2] || "🎀𝓜𝓐𝓡𝓘𝓝𝓔🎀",
 						quality,
 						type: StickerTypes[
 							parsedArgs.flags.includes("--crop") ||
