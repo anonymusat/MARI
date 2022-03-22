b30e712b627b519852749e75642280c1b0e6ca76
DEVILL-MASCOT <102029388+DEVILL-MASCOT@users.noreply.github.com>
2022-03-22 15:13:06 +0530

    Update Help.ts

--- src/commands/General/Help.ts
+++ src/commands/General/Help.ts
@@ -55,7 +55,7 @@ export default class Command extends BaseCommand {
 │⋊ ℕ𝕒𝕞𝕖: 🎀𝓜𝓐𝓡𝓘𝓝𝓔🎀
 │⋊ ℙ𝕣𝕖𝕗𝕚𝕩: ${this.client.config.prefix}
 │⋊ 𝕆𝕨𝕟𝕖𝕣: ${this.client.config.prefix}mods>
-│⋊ 𝕆𝕗𝕗𝕚𝕔𝕚𝕒𝕝 𝔾𝕣𝕠𝕦𝕡: https://chat.whatsapp.com/E5CwW1dAXjRKE3XuLXxF8J
+│⋊ 𝕆𝕗𝕗𝕚𝕔𝕚𝕒𝕝 𝔾𝕣𝕠𝕦𝕡: https://chat.whatsapp.com/JdCxJkOzJn38TRYq5Q5evp
 ╰────────────┈平和                            \n\n`
             const keys = Object.keys(categories)
             for (const key of keys)
