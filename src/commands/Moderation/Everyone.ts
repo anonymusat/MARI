54d3b862c66bf1c7f8a5f25b86c36f6468c0d867
DEVILL-MASCOT <102029388+DEVILL-MASCOT@users.noreply.github.com>
2022-03-22 15:36:37 +0530

    Update Everyone.ts

--- src/commands/Moderation/Everyone.ts
+++ src/commands/Moderation/Everyone.ts
@@ -22,14 +22,37 @@ export default class Command extends BaseCommand {
 
   run = async (
     M: ISimplifiedMessage,
-    { joined, flags }: IParsedArgs
+    { joined }: IParsedArgs
   ): Promise<void> => {
-    flags.forEach((flag) => (joined = joined.replace(flag, "")));
-    const members = await (
-      await this.client.groupMetadata(M.from)
-    ).participants;
     const stickers = [
-   	    'https://c.tenor.com/Vf6ZPQU3zMoAAAPo/marin-kitagawa-marin.mp4',
+     /** @format */
+
+import MessageHandler from "../../Handlers/MessageHandler";
+import BaseCommand from "../../lib/BaseCommand";
+import WAClient from "../../lib/WAClient";
+import { IParsedArgs, ISimplifiedMessage } from "../../typings";
+import { MessageType, Mimetype } from "@adiwajshing/baileys";
+import { Sticker, Categories, StickerTypes } from "wa-sticker-formatter";
+
+export default class Command extends BaseCommand {
+  constructor(client: WAClient, handler: MessageHandler) {
+    super(client, handler, {
+      command: "everyone",
+      description: "Tags all users in group chat",
+      aliases: ["all", "tagall", "ping"],
+      category: "moderation",
+      usage: `${client.config.prefix}everyone`,
+      adminOnly: true,
+      baseXp: 20,
+    });
+  }
+
+  run = async (
+    M: ISimplifiedMessage,
+    { joined }: IParsedArgs
+  ): Promise<void> => {
+    const stickers = [
+            'https://c.tenor.com/Vf6ZPQU3zMoAAAPo/marin-kitagawa-marin.mp4',
 	    'https://c.tenor.com/gu0EZJfpXP8AAAPo/marin-kitagawa-my-dress-up-darling.mp4',
 	    'https://c.tenor.com/LXLRCmwR9KIAAAPo/kitagawa-marin-marin-kitagawa.mp4',
 	    'https://c.tenor.com/9aXyxmnYW7oAAAPo/my-dress-up-darling-sono-bisque-doll-wa-koi-wo-suru.mp4',
@@ -39,10 +62,11 @@ export default class Command extends BaseCommand {
 	    'https://c.tenor.com/XyfPrGSZizsAAAPo/marin-kitagawa-marin.mp4'
     ];
     const random = stickers[Math.floor(Math.random() * stickers.length)];
-    if (flags.includes("--s") || flags.includes("--sticker")) {
+    const term = joined.trim().split(" ");
+    if (term[0] === "--s" || term[0] === "--sticker") {
       const sticker: any = await new Sticker(random, {
         pack: "READ QUOTED MESSAGE",
-        author: "✴🎀𝓜𝓐𝓡𝓘𝓝𝓔🎀✴",
+        author: "🎀𝓜𝓐𝓡𝓘𝓝𝓔🎀",
         quality: 90,
         type: "default",
         categories: ["🎊"],
@@ -53,11 +77,11 @@ export default class Command extends BaseCommand {
         Mimetype.webp,
         M.groupMetadata?.participants.map((user) => user.jid)
       ));
-    } else if (flags.includes("--h") || flags.includes("--hide")) {
+    } else
       return void (await M.reply(
-        `*🎀 Group: ${M.groupMetadata?.subject}*\n🎏 *Members: ${
-          members.length
-        }*\n📢 *Announcer: @${M.sender.jid.split("@")[0]}*\n🧧 *Tags: HIDDEN*`,
+        `${
+          M.groupMetadata?.subject || "*EVERYONE*"
+        }*HE WANTS TO SAY SOMETHING*\n*@${M.sender.jid.split("@")[0]}*\n*READ QUOTED MESSAGE*\n*[TAGGED MAGICALLY]*`,
         undefined,
         undefined,
         M.groupMetadata?.participants.map((user) => user.jid)
@@ -65,63 +89,36 @@ export default class Command extends BaseCommand {
       ).catch((reason: any) =>
         M.reply(`✖️ An error occurred, Reason: ${reason}`)
       ));
-    } else {
-      interface metadata {
-        mods: string[];
-        admins: string[];
-        others: string[];
-      }
-      const metadata: metadata = {
-        mods: [],
-        admins: [],
-        others: [],
-      };
-      for (const i of members) {
-        if (i.jid === M.sender.jid) continue;
-        if (!this.client.config.mods?.includes(i.jid)) continue;
-        metadata.mods.push(i.jid);
-      }
-      for (const a of members) {
-        if (a.jid === M.sender.jid) continue;
-        if (this.client.config.mods?.includes(a.jid)) continue;
-        if (!a.isAdmin) continue;
-        metadata.admins.push(a.jid);
-      }
-      for (const k of members) {
-        if (k.jid === M.sender.jid) continue;
-        if (this.client.config.mods?.includes(k.jid)) continue;
-        if (k.isAdmin) continue;
-        metadata.others.push(k.jid);
-      }
-      let text = `*🎀 Group: ${M.groupMetadata?.subject}*\n🎏 *Members: ${
-        members.length
-      }*\n📢 *Announcer: @${M.sender.jid.split("@")[0]}*\n🧧 *Tags:*`;
-      if (metadata.mods.length > 0) {
-	      text += `\n`;
-        for (const Mods of metadata.mods) {
-		text += `\n🏅 *@${Mods.split("@")[0]}*`;
-        }
-      }
-     // text += `\n`;
-      if (metadata.admins.length > 0) {
-	      text += `\n`;
-        for (const admins of metadata.admins) {
-		text += `\n👑 *@${admins.split("@")[0]}*`;
-        }
-      }
-     // text += `\n`;
-      if (metadata.others.length > 0) {
-        text += `\n`;
-        for (const others of metadata.others) {
-		text += `\n🎗 *@${others.split("@")[0]}*`;
-        }
-      }
-      return void M.reply(
-        text,
-        MessageType.text,
+  };
+}
+    ];
+    const random = stickers[Math.floor(Math.random() * stickers.length)];
+    const term = joined.trim().split(" ");
+    if (term[0] === "--s" || term[0] === "--sticker") {
+      const sticker: any = await new Sticker(random, {
+        pack: "READ QUOTED MESSAGE",
+        author: "ASUNA🍺",
+        quality: 90,
+        type: "default",
+        categories: ["🎊"],
+      });
+      return void (await M.reply(
+        await sticker.build(),
+        MessageType.sticker,
+        Mimetype.webp,
+        M.groupMetadata?.participants.map((user) => user.jid)
+      ));
+    } else
+      return void (await M.reply(
+        `${
+          M.groupMetadata?.subject || "*EVERYONE*"
+        }\n*READ QUOTED MESSAGE*\n*[TAGGED MAGICALLY]*`,
+        undefined,
         undefined,
         M.groupMetadata?.participants.map((user) => user.jid)
-      );
-    }
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      ).catch((reason: any) =>
+        M.reply(`✖️ An error occurred, Reason: ${reason}`)
+      ));
   };
 }
