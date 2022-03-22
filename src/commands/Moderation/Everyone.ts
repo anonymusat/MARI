d619e528b2cd396231e0f7825129578c54d1451e
DEVILL-MASCOT <102029388+DEVILL-MASCOT@users.noreply.github.com>
2022-03-22 13:06:49 +0530

    Update Everyone.ts

--- src/commands/Moderation/Everyone.ts
+++ src/commands/Moderation/Everyone.ts
@@ -22,14 +22,18 @@ export default class Command extends BaseCommand {
 
   run = async (
     M: ISimplifiedMessage,
-    { joined }: IParsedArgs
+    { joined, flags }: IParsedArgs
   ): Promise<void> => {
+    flags.forEach((flag) => (joined = joined.replace(flag, "")));
+    const members = await (
+      await this.client.groupMetadata(M.from)
+    ).participants;
     const stickers = [
-     "https://wallpapercave.com/w/wp6139612.jpg",
+   		        "https://wallpapercave.com/w/wp6139612.jpg",
    		        "https://wallpapercave.com/w/wp8879925.jpg",
           		"https://wallpapercave.com/w/wp6139586.jpg",
           		"https://wallpapercave.com/wp/wp6139586.jpg",
-         	     "https://wallpapercave.com/wp/wp10472356.png",
+         	        "https://wallpapercave.com/wp/wp10472356.png",
           		"https://wallpapercave.com/wp/wp10472348.jpg",
           		"https://wallpapercave.com/wp/wp8108753.jpg",
           		"https://wallpapercave.com/wp/wp10440387.jpg",
@@ -37,8 +41,7 @@ export default class Command extends BaseCommand {
 			"https://wallpapercave.com/wp/wp9078688.jpg",
     ];
     const random = stickers[Math.floor(Math.random() * stickers.length)];
-    const term = joined.trim().split(" ");
-    if (term[0] === "--s" || term[0] === "--sticker") {
+    if (flags.includes("--s") || flags.includes("--sticker")) {
       const sticker: any = await new Sticker(random, {
         pack: "READ QUOTED MESSAGE",
         author: "✴🎀𝓜𝓐𝓡𝓘𝓝𝓔🎀✴",
@@ -52,11 +55,11 @@ export default class Command extends BaseCommand {
         Mimetype.webp,
         M.groupMetadata?.participants.map((user) => user.jid)
       ));
-    } else
+    } else if (flags.includes("--h") || flags.includes("--hide")) {
       return void (await M.reply(
-        `${
-          M.groupMetadata?.subject || "*EVERYONE*"
-        }\n*TAGGER :*${M.sender.username}* \n*THE PERSON WANTS TO SAY SOMETHING*\n*[INBUILT TAG]*`,
+        `*🎀 Group: ${M.groupMetadata?.subject}*\n🎏 *Members: ${
+          members.length
+        }*\n📢 *Announcer: @${M.sender.jid.split("@")[0]}*\n🧧 *Tags: HIDDEN*`,
         undefined,
         undefined,
         M.groupMetadata?.participants.map((user) => user.jid)
@@ -64,5 +67,62 @@ export default class Command extends BaseCommand {
       ).catch((reason: any) =>
         M.reply(`✖️ An error occurred, Reason: ${reason}`)
       ));
+    } else {
+      interface metadata {
+        mods: string[];
+        admins: string[];
+        others: string[];
+      }
+      const metadata: metadata = {
+        mods: [],
+        admins: [],
+        others: [],
+      };
+      for (const i of members) {
+        if (i.jid === M.sender.jid) continue;
+        if (!this.client.config.mods?.includes(i.jid)) continue;
+        metadata.mods.push(i.jid);
+      }
+      for (const a of members) {
+        if (a.jid === M.sender.jid) continue;
+        if (this.client.config.mods?.includes(a.jid)) continue;
+        if (!a.isAdmin) continue;
+        metadata.admins.push(a.jid);
+      }
+      for (const k of members) {
+        if (k.jid === M.sender.jid) continue;
+        if (this.client.config.mods?.includes(k.jid)) continue;
+        if (k.isAdmin) continue;
+        metadata.others.push(k.jid);
+      }
+      let text = `*🎀 Group: ${M.groupMetadata?.subject}*\n🎏 *Members: ${
+        members.length
+      }*\n📢 *Announcer: @${M.sender.jid.split("@")[0]}*\n🧧 *Tags:*`;
+      if (metadata.mods.length > 0) {
+        for (const Mods of metadata.mods) {
+          text += `\n🏅 *@${Mods.split("@")[0]}*`;
+        }
+      }
+     // text += `\n`;
+      if (metadata.admins.length > 0) {
+        text += `\n`;
+        for (const admins of metadata.admins) {
+          text += `\n👑 *@${admins.split("@")[0]}*`;
+        }
+      }
+     // text += `\n`;
+      if (metadata.others.length > 0) {
+        text += `\n`;
+        for (const others of metadata.others) {
+          text += `\n🎗 *@${others.split("@")[0]}*`;
+        }
+      }
+      return void M.reply(
+        text,
+        MessageType.text,
+        undefined,
+        M.groupMetadata?.participants.map((user) => user.jid)
+      );
+    }
   };
 }
